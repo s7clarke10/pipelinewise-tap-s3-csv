6d1284280bc070a58f4347ae03594d38b06f21c0
Steve Clarke <84364906+s7clarke10@users.noreply.github.com>
2024-08-26 07:07:38 +0000

    Working tox pipeline

--- run_integration_tests.py
+++ run_integration_tests.py
@@ -1,11 +1,59 @@
-#!/bin/env/python
-
 import os
 
-from subprocess import call
+import docker
+import logging
+
+from subprocess import call, Popen, run
+
+# check_container.py
+from typing import Optional
+
+
+
+def is_container_running(container_name: str) -> Optional[bool]:
+    """Verify the status of a container by it's name
+
+    :param container_name: the name of the container
+    :return: boolean or None
+    """
+    RUNNING = "running"
+    # Connect to Docker using the default socket or the configuration
+    # in your environment
+    docker_client = docker.from_env()
+    # Or give configuration
+    # docker_socket = "unix://var/run/docker.sock"
+    # docker_client = docker.DockerClient(docker_socket)
+
+    try:
+        container = docker_client.containers.get(container_name)
+    except docker.errors.NotFound as exc:
+        print(f"Check container name!\n{exc.explanation}")
+    else:
+        container_state = container.attrs["State"]
+        return container_state["Status"] == RUNNING
+
+
+def run_command(command, env=None):
+    """ Runs requested process with arguments.
+        Return: returncode of executed program.
+    """
+    logging.debug("Command: {}".format(command))
+    result = run(command, env=env, shell=False, capture_output=False)
 
 
 if __name__ == '__main__':
-    if 'GITHUB_ACTIONS' in os.environ:
-        rc = call('pytest tests/integraion')
+    container_name = "minio_server"
+    result = is_container_running(container_name)
+    print(result)
+    print(os.environ)
+    if result or 'GITHUB_ACTIONS' in os.environ:
+        rc = run_command(['poetry', 'run', 'pytest', 'tests/integration'],env=os.environ)
         raise SystemExit(rc)
+    else:
+        print('Skipping Integration Tests - minio is not running')
+        print('-------------------------------------------------')
+        print('Ensure minio server is running to run these tests.')
+        print('You can start minio server by these steps:')
+        print('docker compose up -d')
+        print('poetry run python run_integration_tests.py')
+
